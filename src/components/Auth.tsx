889d021e923ca802e019d1d7ca19a8f085d1fc03
Rex <your_username@users.noreply.github.com>
2026-04-19 21:56:16 +0530

    fix: resolve build error by correcting medium-common import and cleaning up config

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,6 +1,6 @@
 import { ChangeEvent, useState } from "react";
 import { Link, useNavigate } from "react-router-dom"
-import { SignupInput } from "@instructiveagonizing/medium-common";
+import { SignupInput } from "@100xdevs/medium-common";
 import axios from "axios";
 import { BACKEND_URL } from "../config"
 
